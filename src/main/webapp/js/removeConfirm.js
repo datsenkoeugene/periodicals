8e29cdd67db1c41ce35f36ae23671ac202331335
Yevhen Datsenko <eugene.datsenko.io@gmail.com>
2021-09-14 00:16:52 +0300

    feature removeConfirm.js

---
+++ src/main/webapp/js/removeConfirm.js
@@ -0,0 +1,26 @@
+import {
+    EDIT_PUBLICATION,
+    DELETE_PUBLICATION,
+    PERIODICALS_LIST,
+    language
+} from './constants.js';
+
+export const removeConfirm = selector => {
+    const publicationTable = document?.querySelector(selector);
+    publicationTable?.addEventListener('click', e => {
+        e.preventDefault();
+        const { btn } = e.target.dataset;
+        const { id } = e.target;
+        const { lang } = document.documentElement;
+        if (btn === 'editBtn') {
+            location.href = `${EDIT_PUBLICATION}&id=${id}`
+        } else if (btn === 'removeBtn') {
+            const question = lang === 'ru' ? language[lang] : language['en'];
+            const isRemove = confirm(question);
+            if (isRemove) {
+                location.href = `${DELETE_PUBLICATION}&id=${id}`;
+            }
+            location.href = `${PERIODICALS_LIST}`;
+        }
+    })
+}
