28da0f1fb09ae47e78d8593deb999da60f007787
Yevhen Datsenko <eugene.datsenko.io@gmail.com>
2021-09-14 00:19:45 +0300

    refactor LoginCommand

--- src/main/java/com/eugenedatsenko/web/command/authorization/LoginCommand.java
+++ src/main/java/com/eugenedatsenko/web/command/authorization/LoginCommand.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.List;
 import org.apache.log4j.Logger;
 
 /**
