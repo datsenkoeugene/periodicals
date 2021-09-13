5c55e67e750b1dfaf1054a78314bfe7d8e83ea96
Yevhen Datsenko <eugene.datsenko.io@gmail.com>
2021-09-13 17:16:07 +0300

    delete clearAlert.js

--- src/main/webapp/js/clearAlert.js
+++
@@ -1,6 +0,0 @@
-export const clearAlert = selector => {
-    const alertInfo = document.querySelector(selector);
-    setTimeout(() => {
-        alertInfo?.parentNode.removeChild(alertInfo);
-    }, 3000);
-}
