22399c4d6db1041e7379797d6f4eb704373bcec4
Yevhen Datsenko <eugene.datsenko.io@gmail.com>
2021-09-14 00:17:01 +0300

    feature constants.js

---
+++ src/main/webapp/js/constants.js
@@ -0,0 +1,8 @@
+export const EDIT_PUBLICATION = '/controller?command=editPublication';
+export const DELETE_PUBLICATION = '/controller?command=deletePublication';
+export const PERIODICALS_LIST = '/controller?command=listPeriodicals';
+
+export const language = {
+    ru: `Вы действительно хотите удалить эту публикацию ?`,
+    en: `Are you sure you want to delete this periodical ?`
+}
