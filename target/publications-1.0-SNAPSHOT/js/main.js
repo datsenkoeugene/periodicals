706cb05f3a7423e80a5c89806d911f77f4b2ad45
Yevhen Datsenko <eugene.datsenko.io@gmail.com>
2021-09-16 19:31:57 +0300

    refactor main.js

--- target/publications-1.0-SNAPSHOT/js/main.js
+++ target/publications-1.0-SNAPSHOT/js/main.js
@@ -1,11 +1,11 @@
 import {validateForm} from './validateForm.js';
 import {removeConfirm} from './removeConfirm.js';
+import {changeActiveLink} from './changeActiveLink.js';
 
 console.log(`%cInit App`, `color: lime;`);
 
 validateForm('.needs-validation');
 
-removeConfirm('#publicationTable');
+removeConfirm('#publicationTableBody');
 
-
-
+changeActiveLink('.page-item');
