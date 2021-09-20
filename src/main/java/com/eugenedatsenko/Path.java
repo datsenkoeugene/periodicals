9cfe754ed4ecc1a230431048bf2081de52c673a2
Yevhen Datsenko <eugene.datsenko.io@gmail.com>
2021-09-20 20:45:42 +0300

    feature Path

--- src/main/java/com/eugenedatsenko/Path.java
+++ src/main/java/com/eugenedatsenko/Path.java
@@ -10,6 +10,7 @@ public class Path {
     public static final String PAGE_INSERT_PUBLICATION = "/WEB-INF/jsp/admin/add_publication.jsp";
     public static final String PAGE_EDIT_PUBLICATION = "/WEB-INF/jsp/admin/edit_publication.jsp";
     public static final String PAGE_SEARCH_PUBLICATIONS = "/WEB-INF/jsp/search_publications.jsp";
+    public static final String PAGE_USER_ACCOUNT = "/WEB-INF/jsp/user/user_accounts.jsp";
 
     public static final String COMMAND_USERS_LIST = "/controller?command=listUsers";
     public static final String COMMAND_LOCK_UNLOCK_USER = "/controller?command=lockUnlockUser";
@@ -19,4 +20,6 @@ public class Path {
     public static final String COMMAND_DELETE_PUBLICATION = "/controller?command=deletePublication";
     public static final String COMMAND_EDIT_PUBLICATION = "/controller?command=editPublication";
     public static final String COMMAND_SORT_PUBLICATION_BY_NAME = "/controller?command=sortByName";
+
+    public static final String COMMAND_USER_ACCOUNT = "/controller?command=userAccount";
 }
